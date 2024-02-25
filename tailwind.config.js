35d38ca6e4b4eab55b629b17a79ee32f74fcce85
Angel Garbarino <argarbarino@gmail.com>
2024-02-25 09:56:43 -0700

    adding tailwind and prettier.

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+  },
+  plugins: [],
+}
